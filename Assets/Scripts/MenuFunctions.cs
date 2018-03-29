081fdadef6943617a8e1d24cf2723eee173209aa
Unknown <mario.shadic.esteves@hotmail.com>
2018-03-29 18:08:10 +0100

    started menu, some functionality added.

---
+++ Assets/Scripts/MenuFunctions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[AddComponentMenu("Game Menu/Menu Functionality")]
+public class MenuFunctions : MonoBehaviour {
+
+    [SerializeField]
+    GameObject mainMenuScreen, levelSelectScreen, creditsScreen, encyclopediaScreen;
+
+    public void PlayGame(int levelBuildIndex)
+    {
+        SceneManager.LoadScene(levelBuildIndex);
+    }
+
+    public void SetLevelSelectVisibility(bool isVisible)
+    {
+        if(levelSelectScreen)
+            levelSelectScreen.SetActive(isVisible);
+    }
+
+    public void SetCreditsVisibility(bool isVisible)
+    {
+        if(creditsScreen)
+            creditsScreen.SetActive(isVisible);
+    }
+
+    public void SetEncyclopediaVisibility(bool isVisible)
+    {
+        if(encyclopediaScreen)
+            encyclopediaScreen.SetActive(isVisible);
+    }
+
+    public void SetMainMenuScreenVisiblity(bool isVisible)
+    {
+        if (mainMenuScreen)
+            mainMenuScreen.SetActive(isVisible);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
