a589833184edd222b446511152130783f795b6cc
Pedro Daniel Reis <up201506046@fe.up.pt>
2018-04-07 22:18:12 +0100

    Started Jacintas, already reproducing

---
+++ Assets/Scripts/Jacinta.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jacinta : MonoBehaviour {
+
+	public int Health = 0;
+	public float secondsToDry = 1f;
+	public float secondsToReproduce;
+
+	private JacintaManager manager;
+
+	private Color jacintaColor;
+	
+	private enum JacintaState {
+		DRYING, WATERED
+	}
+
+	// Use this for initialization
+	void Start () {
+		jacintaColor = new Color(1,1,1,1); // Updates when dried
+		secondsToDry = 1;
+		secondsToReproduce = Random.Range(1f, 2f);
+		GameObject gameManager = GameObject.Find("GameManager");
+		manager = (JacintaManager) gameManager.GetComponent(typeof(JacintaManager));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		secondsToReproduce -= Time.deltaTime;
+		if (secondsToReproduce <= 0) {
+			reproduce();
+			secondsToReproduce = Random.Range(1f, 2f);
+		}
+	}
+
+	void reproduce() {
+		manager.updateNumberJacintas();
+		Instantiate(this, new Vector3(this.transform.position.x + Random.Range(-1f,1f), 
+				this.transform.position.y + Random.Range(-0.3f, 0.3f), this.transform.position.z), Quaternion.identity);
+	}
+}
--- Assets/Scripts/JacintaManager.cs
+++ Assets/Scripts/JacintaManager.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class JacintaManager : MonoBehaviour {
 
+	private SpriteRenderer waterRenderer;
+	public float health;
+	public int numberJacintas;
+
+	private float rWater, gWater, bWater; // RGB Components for dead water
+	public Color waterColor;
+	
 	// Use this for initialization
 	void Start () {
-		
+		// WATER
+		waterRenderer = GameObject.Find("Water").GetComponent<SpriteRenderer>();
+		health = 100f;
+
+		rWater=45; gWater=71; bWater=58; // Change here to alter water tint
+
+		waterColor = new Color(1,1,1,1);				
+		// JACINTAS
+
+		numberJacintas = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		updateWater();
+		//(53, 86, 70)
+	}
+
+	void updateWater() {
+		health -= 0.02f*numberJacintas;
+		waterColor.r = (rWater + (100-rWater)*health/100f)/100f;
+		waterColor.g = (gWater + (100-gWater)*health/100f)/100f;
+		waterColor.b = (bWater + (100-bWater)*health/100f)/100f;
 		
+		waterRenderer.color= waterColor;
+		
+	}
+
+	public void updateNumberJacintas() {
+		numberJacintas++;
 	}
 }
