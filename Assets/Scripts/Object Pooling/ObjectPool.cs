5276acd80e3322ad445ef45e48f76ff86afba279
Unknown <mario.shadic.esteves@hotmail.com>
2018-04-19 12:42:33 +0100

    generic object pooler, plant object pooler uses 2 different pools.

---
+++ Assets/Scripts/Object Pooling/ObjectPool.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ObjectPooling
+{
+
+    /// <summary>
+    /// A gameObject pool. Can be used to hold multiple inactive gameObjects at a time. Mainly used for optimizing gameplay (reduces the number of instantiate and destroy calls.)
+    /// </summary>
+    public class ObjectPool : MonoBehaviour
+    {
+        /// <summary>
+        /// The GameObject to spawn into the pool
+        /// </summary>
+        public GameObject m_gameObjectPrefab;
+
+        /// <summary>
+        /// The size of the object pool (how many will be spawned initially)
+        /// </summary>
+        public int m_objectPoolSize;
+
+        /// <summary>
+        /// The list that holds the object pool.
+        /// </summary>
+        private List<GameObject> m_gameObjectPool;
+        
+        /// <summary>
+        /// Returns an object from this pool.
+        /// </summary>
+        /// <returns>The first inactive object.</returns>
+        public GameObject GetObjectFromPool()
+        {
+            if (m_gameObjectPool.Count == 0)
+                Debug.LogError("This object pool is empty.");
+
+            foreach(GameObject gameObj in m_gameObjectPool)
+                if(!gameObj.activeInHierarchy)
+                    return gameObj;
+            return null;
+        }
+
+        /// <summary>
+        /// Create the objects for this pool.
+        /// </summary>
+        public void GenerateObjects()
+        {
+            if(!m_gameObjectPrefab)
+            {
+                throw new System.NullReferenceException("No game object prefab assigned to this object pool component.");
+            }
+            else if(m_objectPoolSize <= 0)
+            {
+                throw new System.ArgumentException("Invalid object pool size. Will not generate.");
+            }
+
+            for (int i = 0; i < m_objectPoolSize; i++)
+            {
+                GameObject currentObject = Instantiate(m_gameObjectPrefab);
+                currentObject.SetActive(false);
+                m_gameObjectPool.Add(currentObject);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Object Pooling/PlantObjectPooling.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Object Pooling/Plant Object Pool")]
+public class PlantObjectPooler : MonoBehaviour
+{
+    /// <summary>
+    /// The instance to be used by all 
+    /// </summary>
+    public static PlantObjectPooler sharedInstance;
+
+    public ObjectPooling.ObjectPool m_invadingPlantsPool, m_nativePlantsPool;
+        
+    void Awake()
+    {
+        if(!sharedInstance)
+            sharedInstance = this;
+        else
+        {
+            Debug.LogWarning("Found another object pooler in the scene. Destroying the instance on gameObj: " + gameObject.name);
+            Destroy(this);
+        }
+
+        if (!m_nativePlantsPool)
+            Debug.LogError("No native plant pool created!");
+        else
+            m_nativePlantsPool.GenerateObjects();
+
+        if (!m_invadingPlantsPool)
+            Debug.LogError("No invating plant to instantiate!");
+        else
+            m_invadingPlantsPool.GenerateObjects();
+    }
+        
+    public GameObject GetNativePlant()
+    {
+        return m_nativePlantsPool.GetObjectFromPool();
+    }
+
+    public GameObject SpawnNativePlantAtPosition(Vector2 plantPos)
+    {
+        GameObject nativePlant = GetNativePlant();
+        if (nativePlant)
+            nativePlant.transform.position = plantPos;
+        return nativePlant;
+    }
+
+    public GameObject GetInvadingPlant()
+    {
+        return m_invadingPlantsPool.GetObjectFromPool();
+    }
+
+    public GameObject SpawnInvadingPlantAtPosition(Vector2 plantPos)
+    {
+        GameObject invadingPlant = GetInvadingPlant();
+        if (invadingPlant)
+            invadingPlant.transform.position = plantPos;
+        return invadingPlant;
+    }
+
+}
