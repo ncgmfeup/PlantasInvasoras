590f1ac76d307bdf5f32e410e6892320f30a0994
Ricardo Teixeira <ricardomft@gmail.com>
2018-04-28 16:09:20 +0100

    Added fishes to jacinta's lake + main menu changes

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -14,6 +14,48 @@ public class MenuFunctions : MonoBehaviour {
         SceneManager.LoadScene(levelName);
     }
 
+    public void SetMenuScreen(string menu)
+    {
+        SetMainMenuScreenVisiblity(false);
+        SetLevelSelectVisibility(false);
+        SetEncyclopediaVisibility(false);
+        SetCreditsVisibility(false);
+        switch (menu)
+        {
+            case "main menu":
+                SetMainMenuScreenVisiblity(true);
+                SetLevelSelectVisibility(false);
+                SetEncyclopediaVisibility(false);
+                SetCreditsVisibility(false);
+                break;
+            case "level select":
+                SetMainMenuScreenVisiblity(false);
+                SetLevelSelectVisibility(true);
+                SetEncyclopediaVisibility(false);
+                SetCreditsVisibility(false);
+                break;
+            case "enciclopedia":
+                SetMainMenuScreenVisiblity(false);
+                SetLevelSelectVisibility(false);
+                SetEncyclopediaVisibility(true);
+                SetCreditsVisibility(false);
+                break;
+            case "creditos":
+                SetMainMenuScreenVisiblity(false);
+                SetLevelSelectVisibility(false);
+                SetEncyclopediaVisibility(false);
+                SetCreditsVisibility(true);
+                break;
+            default:
+                Debug.Log("Error! Unrecognized menu!");
+                SetMainMenuScreenVisiblity(true);
+                SetLevelSelectVisibility(false);
+                SetEncyclopediaVisibility(false);
+                SetCreditsVisibility(false);
+                break;
+        }
+    }
+
     public void SetMainMenuScreenVisiblity(bool isVisible)
     {
         if (mainMenuScreen)
